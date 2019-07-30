6f510b1a1f1c100acdd574245eee4ec76961bdb1
wesley <tgfc040@tgfc.tw>
2019-07-30 17:54:29 +0800

    import boostrapt & axios api

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Login</h2>
+    <div>
+      <div>{{msg}}</div>
+      <!--            <div>-->
+      <!--                <input type="text" id="account" placeholder="Account" v-model="user.account"/>-->
+      <!--            </div>-->
+      <!--            <div>-->
+      <!--                <input type="password" id="password" placeholder="Password" v-model="user.password"/>-->
+      <!--            </div>-->
+      <div>
+        <!--                <button @click="login">Login</button>-->
+        <b-button>Login</b-button>
+        <h2>123</h2>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+  import axiox from 'axios'
+
+  export default {
+    data() {
+      return {
+        msg: ''
+      }
+    },
+    mounted() {
+      axiox.get('/hello').then(response => {
+        console.log(response);
+        this.msg = response.data;
+      })
+    }
+  }
+
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,35 +1,45 @@
 <template>
-  <div>
-    <h2>Login</h2>
-    <div>
-        <div>{{msg}}</div>
-      <!-- <div>
-        <input type="text" id="account" placeholder="Account" v-model="user.account" />
-      </div>
-      <div>
-        <input type="password" id="password" placeholder="Password" v-model="user.password" />
-      </div>
-      <div>
-        <button @click="login">Login</button>
-      </div> -->
+    <div class="login">
+        <h2>Login</h2>
+        <div>
+            <div>{{msg}}</div>
+            <div>
+<!--                <b-form @submit="onSubmit">-->
+                    <b-form-input v-model="username" placeholder="請輸入帳號"></b-form-input>
+                    <b-button @click="onSubmit">submit</b-button>
+<!--                </b-form>-->
+            </div>
+
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import axiox from 'axios'
-export default{
-    data(){
-        return{
-            msg:''
-        }
-    },
-    mounted(){
-        axiox.get('/hello').then(response =>{
-            console.log(response);
-            this.msg = response.data;
-        })
+    import axiox from 'axios'
+
+    export default {
+        data() {
+            return {
+                username:'',
+                msg: ''
+            }
+        },
+        methods: {
+            onSubmit() {
+                console.log(this.username);
+                axios.post('/helloword',{
+                    'username':this.username
+                }).then(respose => {
+                    console.log(respose.data)
+                })
+            }
+        },
+        // mounted() {
+        //     axiox.get('/hello').then(response => {
+        //         console.log(response);
+        //         this.msg = response.data;
+        //     })
+        // }
     }
-}
 
 </script>
